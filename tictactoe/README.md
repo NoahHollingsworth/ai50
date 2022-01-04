d4a0780979174e3b0d821205bee6fa60259c839e
hollingsworth <hollingsworthuo@gmail.com>
2022-01-03 21:31:21 -0800

    Completed implementation for the functions: player, actions, result, and winner

--- tictactoe/README.md
+++ tictactoe/README.md
@@ -1,5 +1,5 @@
 # Project 0: Tic-Tac-Toe
-Using Minimac, implement an AI to play Tic-Tac-Toe optimally.
+Using Minimax, implement an AI to play Tic-Tac-Toe optimally.
 
 ## Background 
 There are two main files in this project: `runner.py` and `tictactoe.py`. `tictactoe.py` contains all of the logic for playing the game, and for making optimal moves. `runner.py` has been implemented for you, and contains all of the code to run the graphical interface for the game. Once you’ve completed all the required functions in `tictactoe.py`, you should be able to run `python runner.py` to play against your AI!
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+import math, copy 
 
 X = "X"
 O = "O"
@@ -22,28 +22,63 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    countx = sum(x.count(X) for x in board)
+    counto = sum(o.count(O) for o in board)
+    if countx <= counto: #Since X always goes first in this implementation
+        return X 
+    else: 
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    result = set()
+    for i in range(len(board)):
+        for j in range((len(board[i]))):
+            if board[i][j] == EMPTY:
+                result.add((i,j))
+    return result 
+
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    curr_player = player(board)
+    if action not in actions(board): #space is not avaliable
+        raise Exception("Invalid action - this space is not empty")
+    else:
+        #create deepcopy so original board is not alterated until move is actually chosen (in minimax)
+        board_copy = copy.deepcopy(board) 
+        board_copy[action[0]][action[1]] = curr_player
+        return board_copy
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    #check all rows
+    for row in board:
+        if EMPTY not in row and all(x == row[0] for x in row):
+            return row[0]
+    #check all columns
+    for i in range(len(row)):
+        col = [row[i] for row in board]
+        if EMPTY not in row and all(x == col[0] for x in col):
+            return col[0]
+    #check first diagonal
+    diagonal = [board[x][x] for x in range(len(board))]
+    if EMPTY not in diagonal and all(x == diagonal[0] for x in diagonal):
+        return diagonal[0]
+    #check second diagonal
+    diagonal = [board[x][2-x] for x in range(len(board))]
+    if EMPTY not in diagonal and all(x == diagonal[0] for x in diagonal):
+        return diagonal[0]
+
+
 
 
 def terminal(board):
